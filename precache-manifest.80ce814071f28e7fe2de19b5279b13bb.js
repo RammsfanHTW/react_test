f28fbf3093fbb330bb11dde556fb90448e922993
RammsfanHTW <64034634+RammsfanHTW@users.noreply.github.com>
2020-06-25 05:01:30 +0300

    Updates

---
+++ precache-manifest.80ce814071f28e7fe2de19b5279b13bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f87a5c495e3483b730aa1005ae2769e9",
+    "url": "/react__test/index.html"
+  },
+  {
+    "revision": "ecfb710edc0cc625f08d",
+    "url": "/react__test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "82d2ae0125d56accaa2d",
+    "url": "/react__test/static/js/2.906e1148.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react__test/static/js/2.906e1148.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecfb710edc0cc625f08d",
+    "url": "/react__test/static/js/main.dcd77789.chunk.js"
+  },
+  {
+    "revision": "656a08af9520f571a6d9",
+    "url": "/react__test/static/js/runtime-main.830c88e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react__test/static/media/logo.5d5d9eef.svg"
+  }
+]);
